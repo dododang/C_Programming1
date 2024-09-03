d612521753043df2f9620f9aa59cf5af62936427
dodongtak <94369467+dododang@users.noreply.github.com>
2024-09-03 16:14:39 +0900

    Add files via upload
    week01

---
+++ p02-01.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(void) {
+	printf("±èµ¿¾Æ\n±è µ¿ ¾Æ\n±è  µ¿  ¾Æ\n");
+
+	return 0;
+}
---
+++ p02-02.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void) {
+	printf("김동아\n");
+	printf("서울 서대문구 연희로41길 69-12\n");
+	printf("010-4066-7567\n");
+
+	return 0;
+}
